a2b1486b2a91cb51246e52e1ec54985a10fd21ca
Brayan Camargo <brayanochz@gmail.com>
2023-09-22 15:16:10 -0500

    ✨ feat: add card component

--- kitty-web/src/components/card/Card.tsx
+++ kitty-web/src/components/card/Card.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Kitty } from "../../types/kitty";
+import "./index.css";
 
-const card = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+interface props {
+  kitty?: Kitty;
+}
+
+const card: React.FC<props> = (kitty) => {
+  return (
+    <section className={'card'}>
+      {kitty ? (
+        <p>No kitty!</p>
+      ) : (
+        <>
+          <img src={`/images`}/>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default card;
